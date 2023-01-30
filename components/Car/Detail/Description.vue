9fc1d1bf68a9cc2562fb208ddf8723db7475967e
Greg <Greg@2book.com>
2023-01-30 17:38:26 -0500

    Car Details

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="mt-5">
+        <p class="mb-10">
+            The customer is very important, the customer will be followed by the customer. Let's live and enjoy the freedom of the eros. There was no house, no mass, and no members.
+        </p>
+    </div>
+</template>
--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,7 +1,8 @@
 <template>
-    <div>
-        <h1>Dynamic Car Detail Page</h1>
-        <div>{{ name }}</div>
-        <div>{{ id }}</div>
+    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
+        <CarDetailHero />
+        <CarDetailAttributes />
+        <CarDetailDescription />
+        <CarDetailContact />
     </div>
 </template>
