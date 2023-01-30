ed9684b350a09a74801d7d714b6f87b6c120601e
Greg <Greg@2book.com>
2023-01-30 18:35:47 -0500

    Cars Index Gallery using local json data (cars.json)

--- pages/Cars.vue
+++ pages/Cars.vue
@@ -1,13 +1,29 @@
 <template>
     <div class="container mx-auto">
         <div class="mt-32 flex">
-            <!-- CARS SIDEBAR -->
-            <CarSidebar />
+            <div>
+                <h1 class="text-2xl mb-3">Filters</h1>
+                <CarSidebar />
+            </div>
 
             <!-- CARS INDEX -->
             <div class="w-full">
-                <CarCard />
+                <h1 class="text-2xl mb-3">({{ cars?.length }}) Cars</h1>
+                <CarCard
+                    v-for="car in cars"
+                    :key="car.id"
+                    :car="car"
+                />
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+    const { cars } = useCars();
+    const route = useRoute();
+
+    useHead({
+        title: `Cars Index of ${cars.length}`,
+    });
+</script>
