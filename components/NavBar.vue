532609a6b9705a3b148f307799ec3ff31a4877dc
Greg <Greg@2book.com>
2023-01-30 15:17:23 -0500

    Setup Components + Pages

---
+++ components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- NAVBAR -->
+    <header class="
+            sticky
+            top-0
+            z-50
+            flex
+            justify-between
+            items-center
+            space-x-1
+            border-b
+            bg-white
+            p-4
+            shadow-md
+        ">
+        <a href="#" class="text-3xl font-mono">cartrader</a>
+    </header>
+</template>
