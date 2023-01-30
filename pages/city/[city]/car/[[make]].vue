6d9de9249d93a47060b31ba757840f158aa5b2cd
Greg <Greg@2book.com>
2023-01-30 15:58:48 -0500

    Dynamic Page Route: city/[city]/car/[[make]]

---
+++ pages/city/[city]/car/[[make]].vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>City & Make</h1>
+        <div>
+            <div>City: {{city}}</div>
+            <div>Make: {{make}}</div>
+        </div>
+    </div>
+</template>
