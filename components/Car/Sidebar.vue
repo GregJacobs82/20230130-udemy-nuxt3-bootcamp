f39351c5c1320e1c05c21b824a73d161ab77b18d
Greg <Greg@2book.com>
2023-01-30 19:52:02 -0500

    Popup w/ toggle

--- components/Car/Sidebar.vue
+++ components/Car/Sidebar.vue
@@ -2,16 +2,38 @@
     <div class="shadow border w-64 mr-10 z-30 h-[190px]">
 
         <!-- SEARCH LOCATION -->
-        <div class="p-5 flex justify-between relative cursor-pointer border-b">
+        <div class="p-5 flex justify-between relative border-b">
             <h3>Location</h3>
-            <h3 class="text-blue-400 capitalize">{{ city }}</h3>
+            <h3
+                class="text-blue-400 capitalize cursor-pointer"
+                @click="togglePopup"
+            >
+                {{ city }}
+            </h3>
 
             <!-- CHANGE CITY LOCATION -->
-            <div class="hidden absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-                <input type="text" class="border p-1 rounded" />
-                <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
-                    Apply
-                </button>
+            <div
+                class="absolute border shadow-lg left-56 top-1 m-1 bg-white"
+                :class="{ 'hidden':hidePopup }"
+                style="z-index:100;"
+            >
+                <!-- HEADER -->
+                <div class="flex items-center justify-between border-b w-100">
+                    <div class="text-gray-500 p-2">Change Location</div>
+                    <button
+                        class="py-1 px-3 rounded-lg font-bold"
+                        @click="togglePopup"
+                    >
+                        X
+                    </button>
+                </div>
+                <!-- CONTENT -->
+                <div class="p-5">
+                    <input type="text" class="border p-1 rounded" placeholder="City..." />
+                    <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
+                        Apply
+                    </button>
+                </div>
             </div>
         </div>
 
@@ -29,6 +51,12 @@
     </div>
 </template>
 <script setup>
+    const hidePopup = ref(true);
+
+    const togglePopup = () => {
+        hidePopup.value = !hidePopup.value
+    }
+
     const props = defineProps({
         city: {
             type: String,
